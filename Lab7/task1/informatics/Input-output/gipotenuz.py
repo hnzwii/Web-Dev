dd325bb30a2ffc77c75d5f34116786d786daa693
hnzwii <111293530+hnzwii@users.noreply.github.com>
2025-03-23 20:43:26 +0500

    Lab7

---
+++ Lab7/task1/informatics/Input-output/gipotenuz.py
@@ -0,0 +1,8 @@
+from math import *
+
+a = int(input("Print a side: "))
+b = int(input("Print b side: "))
+
+c = sqrt(a**2 + b**2)
+
+print(c)
---
+++ Liho2402 Web-Dev main lab7/task1/informatics/Input-output/apples.py
@@ -0,0 +1,6 @@
+from math import *
+
+n = int(input("Print amount of child: "))
+k = int(input("Print amount of apples: "))
+
+print(floor(k / n))
